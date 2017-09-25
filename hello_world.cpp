ed13aff9f460cbf744eaf344aa889a0838f66de5
Alex <arnischwitz@gmail.com>
2017-09-25 17:44:37 -0400

    test (#1)

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+// hello_world.cpp
+//
+// classic hello world for testing purposes
+//
+// written by alex
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\nHello World\n\n";
+}
